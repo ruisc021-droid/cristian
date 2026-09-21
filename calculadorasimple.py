f5194b5b448db0e17b56602acca01b078a59bbf2
Cristian <ruisc021@gmail.com>
2026-09-21 15:44:36 -0600

    Add calculadora simple

---
+++ calculadorasimple.py
@@ -0,0 +1,13 @@
+#calculadorasimple
+a = float (input("Ingrese numero") ) 
+z =  float (input ("Ingrese otro numero"))
+resultado = a + z
+print (f" El resultado de la suma es {resultado}")
+resultado = a - z
+print(f"El  resultado de la resta es {resultado}")
+resultado = a * z
+print(f"El resultado de la multiplicacion es {resultado}")
+resultado = a / z
+print(f"El resultado de la division es {resultado}")
+
+
